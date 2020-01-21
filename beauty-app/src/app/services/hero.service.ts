385dd17d43abc33a9001055e5bf3ce970a9a85f3
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2020-01-21 12:51:25 -0500

    commit

--- beauty-app/src/app/services/hero.service.ts
+++ beauty-app/src/app/services/hero.service.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class HeroService {
-  private url: any = 'http://localhost/beauty-lab/beauty-api/public/api';
+  private url: any = 'http://localhost:81/appbeautyLab/beauty-api/public';
   private token: string = null;
   constructor(private router: Router) { }
   getUrl() {
