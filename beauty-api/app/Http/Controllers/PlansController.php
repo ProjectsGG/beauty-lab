8c3b0248b01fca170b953420d530ff5ff8718b27
Steven Betancur <steven.betancur@grupomatrix.co>
2020-01-29 21:35:45 -0500

    Actualizacion api

--- beauty-api/app/Http/Controllers/PlansController.php
+++ beauty-api/app/Http/Controllers/PlansController.php
@@ -8,12 +8,6 @@ use App\Models\Plans;
 
 class PlansController extends Controller
 {
-    protected $user;
-
-    public function __construct()
-    {
-        $this->user = JWTAuth::parseToken()->authenticate();
-    }
 
     public function index()
     {
