d7d09f69cc623973b536966e633d5063b46bc42c
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2020-02-02 19:16:16 -0500

    changes

--- beauty-app/src/app/pages/inicio/inicio.page.ts
+++ beauty-app/src/app/pages/inicio/inicio.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MenuController} from '@ionic/angular';
 import { Location } from '@angular/common';
 import { HeroService } from '../../services/hero.service';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 @Component({
   selector: 'app-inicio',
@@ -9,14 +10,27 @@ import { HeroService } from '../../services/hero.service';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
+  loginDetails: any;
   constructor(
     public menuCtrl: MenuController,
     private location: Location,
-    private hero: HeroService) { }
+    private hero: HeroService,
+    private googleplus: GooglePlus) { }
 
   ionViewWillEnter() {
   this.menuCtrl.enable(false);
+   }
+
+   Login() {
+    this.googleplus.login({}).then((res) => {
+      this.loginDetails = res;
+    }, (err) => {
+    });
+   }
+
+   Logout() {
+    this.googleplus.logout();
+    alert('Logged out');
    }
 
   ngOnInit() {
