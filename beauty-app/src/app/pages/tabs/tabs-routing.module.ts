373c36199918847e3bc617f8f23d9e50ec54b991
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2020-02-03 17:51:00 -0500

    paypal

--- beauty-app/src/app/pages/tabs/tabs-routing.module.ts
+++ beauty-app/src/app/pages/tabs/tabs-routing.module.ts
@@ -8,15 +8,15 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-        { path: 'home', loadChildren: '../homeapp/homeapp.module#HomeappPageModule' },
-        { path: 'shop', loadChildren: '../shop/shop.module#ShopPageModule' },
-        { path: 'social', loadChildren: '../social/social.module#SocialPageModule'},
-        { path: 'car-shop', loadChildren: '../car-shop/car-shop.module#CarShopPageModule'}
+        { path: 'home', loadChildren: './../homeapp/homeapp.module#HomeappPageModule' },
+        { path: 'shop', loadChildren: './../shop/shop.module#ShopPageModule' },
+        { path: 'social', loadChildren: './../social/social.module#SocialPageModule'},
+        { path: 'car-shop', loadChildren: './../car-shop/car-shop.module#CarShopPageModule'}
     ]
   },
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: 'home',
     pathMatch: 'full'
   }
 ];
