be13111bf50328ce4445f4468fa3fabae0cc43df
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2020-02-04 17:48:36 -0500

    Update deposit.page.ts

--- beauty-app/src/app/pages/deposit/deposit.page.ts
+++ beauty-app/src/app/pages/deposit/deposit.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PayPal, PayPalPayment, PayPalConfiguration } from '@ionic-native/paypal/ngx';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-deposit',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private payPal: PayPal, public toastr: ToastService) { }
+  paymentAmount = '3.33';
+  currency = 'USD';
+  currencyIcon = '$';
   ngOnInit() {
   }
+  payWithPaypal() {
+    console.log('Pay ????');
+    this.payPal.init({
+      PayPalEnvironmentProduction: 'ARWrJ5pdmpzKphRRLPijQKobEXbgnqV19iWT_kSSGR8HyRnTxcNAYLFxvN4CwtsDEI6aVqUpOt1QW3BE',
+      PayPalEnvironmentSandbox: 'sb-c6oa43982474@business.example.com'
+    }).then(() => {
+      // Environments: PayPalEnvironmentNoNetwork, PayPalEnvironmentSandbox, PayPalEnvironmentProduction
+      this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+        // Only needed if you get an 'Internal Service Error' after PayPal login!
+        // payPalShippingAddressOption: 2 // PayPalShippingAddressOptionPayPal
+      })).then(() => {
+        const payment = new PayPalPayment(this.paymentAmount, this.currency, 'Description', 'sale');
+        this.payPal.renderSinglePaymentUI(payment).then((res) => {
+          this.toastr.success('Successful payment');
+          // Successfully paid
 
+          // Example sandbox response
+          //
+          // {
+          //   'client': {
+          //     'environment': 'sandbox',
+          //     'product_name': 'PayPal iOS SDK',
+          //     'paypal_sdk_version': '2.16.0',
+          //     'platform': 'iOS'
+          //   },
+          //   'response_type': 'payment',
+          //   'response': {
+          //     'id': 'PAY-1AB23456CD789012EF34GHIJ',
+          //     'state': 'approved',
+          //     'create_time': '2016-10-03T13:33:33Z',
+          //     'intent': 'sale'
+          //   }
+          // }
+        }, () => {
+          // Error or render dialog closed without being successful
+        });
+      }, () => {
+       this.toastr.error('Error in the configuration the PayPal');
+        // Error in configuration
+      });
+    }, () => {
+      this.toastr.error('Paypal initialization failed');
+      // Error in initialization, maybe PayPal isn't supported or something else
+    });
+  }
 }
