b51d00e77b4ed2c0fd7eb2c83fcfe61b33a77b83
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2020-02-06 21:01:01 -0500

    6Febrero

--- beauty-app/src/app/pages/contact/contact.page.ts
+++ beauty-app/src/app/pages/contact/contact.page.ts
@@ -17,7 +17,7 @@ export class ContactPage implements OnInit {
         icon: 'logo-whatsapp',
         cssClass: 'green-wpp',
         handler: () => {
-          console.log('Wpp clic');
+          location.href = 'https://wa.me/573003408407?text=%20Hi,%20Im%20interested%20in%20BeautyLab';
         }
       }, {
         text: 'Mail',
@@ -31,7 +31,7 @@ export class ContactPage implements OnInit {
         icon: 'cloud-outline',
         cssClass: 'gray-msg',
         handler: () => {
-          console.log('Message clic');
+        console.log('Message clic');
         },
       },
        {
