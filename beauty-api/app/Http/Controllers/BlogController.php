276eadb67c2eb091e0329a0cf86d1faff9cf82c2
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2020-03-06 11:57:30 -0500

    333

--- beauty-api/app/Http/Controllers/BlogController.php
+++ beauty-api/app/Http/Controllers/BlogController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use App\Models\imagesXblog;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use JWTAuth;
+use Illuminate\Support\Facades\Storage;
 class BlogController extends Controller
 {
     protected $user;
@@ -17,7 +17,11 @@ class BlogController extends Controller
     }
     public function index()
     {
-        //
+        $blogs = Blog::orderBy('id','DESC')->with('images')->get();
+        return response()->json([
+            'ok' => true,
+            'data' => $blogs
+        ]);
     }
 
     /**
@@ -95,36 +99,14 @@ class BlogController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
