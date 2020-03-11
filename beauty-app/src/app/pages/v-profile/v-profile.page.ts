b37fea7dae5f95dc4b3cc09811da3ec77556790a
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2020-03-11 14:57:51 -0500

    b

--- beauty-app/src/app/pages/v-profile/v-profile.page.ts
+++ beauty-app/src/app/pages/v-profile/v-profile.page.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../../services/hero.service';
 import { ActivatedRoute } from '@angular/router';
+import { BlogService } from '../../services/blog.service';
 @Component({
   selector: 'app-v-profile',
   templateUrl: './v-profile.page.html',
   styleUrls: ['./v-profile.page.scss'],
 })
 export class VProfilePage implements OnInit {
+  data: any[];
+  user: any;
   content = 'images';
-  id: number;
-  constructor(public hero: HeroService, private route: ActivatedRoute) { }
+  id: string;
+  constructor(public hero: HeroService, private route: ActivatedRoute, private service: BlogService) { }
 
   ngOnInit() {
-    // tslint:disable-next-line: radix
-    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.id = this.route.snapshot.paramMap.get('id');
   }
   segmentChanged(ev: any) {
     this.hero.getDomain();
     this.content = ev.detail.value;
+  }
+  getData(id) {
+    this.service.blogUser(id).subscribe((r: any) => {
+      this.user = r.user;
+      this.data = r.data;
+    });
   }
 }
