a5c3e0973d1f677a9c3258b14af96e022311954f
ProjectsGG <59677710+ProjectsGG@users.noreply.github.com>
2020-03-13 19:22:21 -0500

    ..

--- beauty-app/src/app/services/shopify.service.ts
+++ beauty-app/src/app/services/shopify.service.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RespuestaProducts } from '../interfaces/shopify';
 import { HeroService } from './hero.service';
-import { environment } from 'src/environments/environment';
 
-
-const apikey = environment.apikey;
-const apiPass = environment.apiPass;
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +15,7 @@ export class ShopifyService {
   getProdcutsShop(){
     const httpOptions = {
       headers: new HttpHeaders({
-        'X-Shopify-Storefront-Access-Token': 'c4e4a2414eed48ff01f4355382e19bd3'
+        'Content-Type': 'application/json',
       })
     };
     return this.http.get<RespuestaProducts>(`https://844672ed6d0f31d8ad2fb844a0636e80:c4e4a2414eed48ff01f4355382e19bd3@beauty-lab-merch.myshopify.com/admin/api/2020-01/collections/173830930491/products.json`, httpOptions);
