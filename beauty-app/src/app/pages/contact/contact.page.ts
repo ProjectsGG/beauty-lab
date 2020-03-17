5975ae49d26f9a24947ff1b550cb069e45e8118b
ProjectsGG <59677710+ProjectsGG@users.noreply.github.com>
2020-03-17 10:32:54 -0500

    contact..

--- beauty-app/src/app/pages/contact/contact.page.ts
+++ beauty-app/src/app/pages/contact/contact.page.ts
@@ -10,7 +10,7 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
 })
 export class ContactPage implements OnInit {
 
-  email = '';
+  subject = '';
   message = '';
   constructor( 
     public socialMedia: ActionSheetController,
@@ -25,7 +25,7 @@ send(){
     cc:[],
     bcc:[],
     attachment:[],
-    subjet:"Message from App BeautyLab",
+    subjet:this.subject,
     body:this.message,
     IsHtml:false,
     app: "Gmail"
