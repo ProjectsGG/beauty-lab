3ab8c599ed932d4fe0929cc2b8f59b66fa83a45d
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2020-03-18 16:00:45 -0500

    Update car-shop.page.ts

--- beauty-app/src/app/pages/car-shop/car-shop.page.ts
+++ beauty-app/src/app/pages/car-shop/car-shop.page.ts
@@ -16,10 +16,8 @@ export class CarShopPage implements OnInit {
   ngOnInit() {
     this.hero.shoppingcart.forEach(e => {
       if (e.procedures.length > 0) {
-        console.log(e.procedures[0].precio);
         this.total = this.total + e.procedures[0].precio;
       } else if (e.plans.length > 0) {
-        console.log(e.plans[0].valor);
         this.total = this.total + e.plans[0].valor;
       }
     });
