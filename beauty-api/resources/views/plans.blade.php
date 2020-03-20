28044de6b382f7885839b4a9a41f98ab4ef27fce
Miguelhito <57653360+miguelhito54@users.noreply.github.com>
2020-03-20 14:49:21 -0500

    .

--- beauty-api/resources/views/plans.blade.php
+++ beauty-api/resources/views/plans.blade.php
@@ -1,9 +1,4 @@
 @extends('layout.app')
 @section('content')
-<div col-12 col-sm-12 col-md-6 col-xl-6>
-    <img class="rounded float-left" src="" alt="">
-</div>
-<div>
-    Description 
-</div>
+
 @endsection
