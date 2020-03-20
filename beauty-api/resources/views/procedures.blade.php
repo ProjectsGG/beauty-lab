f0d78125a9374356ccf4f5f726438a5bc339c6f6
Miguelhito <57653360+miguelhito54@users.noreply.github.com>
2020-03-20 16:00:40 -0500

    procedures webpage

--- beauty-api/resources/views/procedures.blade.php
+++ beauty-api/resources/views/procedures.blade.php
@@ -1,4 +1,149 @@
 @extends('layout.app')
 @section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 p-2 bg-dark">
+            <h1 class="text-center text-light titles">P R O C E D U R E S</h1>
+        </div>
+    </div>
+</div><br>
+<div class="center text-center"><img src="{{ asset('img/whiteLogoH.png') }}" class="logo" alt=""></div>
+<br>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h3 class="text-center text-light subtitles">This are ours procedures</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/breast.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Breast</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">is surgery to increase breast size. It involves placing
+                      breast implants under breast tissue or chest muscles.</li>
+                </ul>
+              </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/cheeksexp.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Cheecks</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">is surgery to increase breast size. It involves placing
+                      breast implants under breast tissue or chest muscles.</li>
+                </ul>
+              </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/botoxexp.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Botox</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">Substance injected into the skin which is known 
+                      to inhibit muscle movement, and that can prevent wrinkles from developing or worsening.</li>
+                </ul>
+              </div>
+        </div>
+    </div>
+    <br>
+    <hr class="bg-light"><hr class="bg-light">
+    <br>
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/eyebrown.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Eyebrowns</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light"> is a cosmetic procedure where hair grafts (plugs) are transferred to your brow area. 
+                      This goal is that new hairs will grow from these grafts, producing a fuller look</li>
+                </ul>
+              </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/chinexp.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Chin</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light"> is a surgical procedure to reshape the chin either 
+                      by enhancement with an implant or reduction surgery on the bone</li>
+                </ul>
+              </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/absexp.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Abs</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">Abdominal etching is done with a special tumescent
+                       liposuction technique. The procedure removes and sculpts fatty tissue residing under the skin. Before the procedure, detailed markings are made of the patient's flexed abs.</li>
+                </ul>
+              </div>
+        </div>
+    </div>
+    <br>
+    <hr class="bg-light"><hr class="bg-light">
+    <br>
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/360lipo.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">360 lipo</h5>
+                  <p class="card-text text-light">This procedures has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">is an all-inclusive liposuction procedure
+                       that is used to contour and shape the midsection, 360 degrees around. </li>
+                </ul>
+              </div>
+        </div>
+    <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/bootyexp.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Chin</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">is a surgical procedure that comprises liposuction of the abdomen, flanks, 
+                      and back. The procedure is conducted 360 degrees around a specific body part. Patients who have excess fat deposits in these areas can benefit from this procedure.</li>
+                </ul>
+              </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+            <div class="card bg-transparent">
+                <img src="{{ asset('img/teethsexp.png') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title text-light">Theet</h5>
+                  <p class="card-text text-light">This procedure has for you</p>
+                </div>
+                <ul class="list-group list-group-flush bg-transparent">
+                  <li class="list-group-item bg-transparent text-light">is surgery that is performed on the tissues of your mouth, 
+                      including your teeth and gums, or, in the case of maxillofacial surgery, in your jaw, head, or face.</li>
+                </ul>
+              </div>
+        </div>
+</div>
 
 @endsection
