270152db0928af4a38d136e9250444172891d615
ProjectsGG <59677710+ProjectsGG@users.noreply.github.com>
2020-03-20 17:01:32 -0500

    contact

--- beauty-api/resources/views/Contact.blade.php
+++ beauty-api/resources/views/Contact.blade.php
@@ -1,6 +1,16 @@
 @extends('layout.app')
 @section('content')
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 p-2 bg-dark">
+            <h1 class="text-center text-light titles">C O N T A C T</h1>
+        </div>
+    </div>
+</div><br>
+<div class="center text-center"><img src="{{ asset('img/whiteLogoH.png') }}" class="logo" alt=""></div>
 <!--FORM CONTACT-->
+<br><br>
 <form>
   <div class="container" display:flex margin-top:2rem>
     <div class="row justify-content-md-center">
@@ -8,14 +18,16 @@
         <div class="row min-padding">
         <input type="text" class="form-control input-lg" placeholder="Enter Your Subject" aria-label="Subject" aria-describedby="addon-wrapping">
         </div>
+        <br>
         <div class="row">
             <textarea rows="8" class="form-control input-lg" id="textarea" placeholder="What is in your mind?" required></textarea>
         </div>
-        </div>      
+        </div>
        <div class="invalid-feedback">
-            Please enter a message in the textarea.
+            Please enter a message
        </div>
       </div>
+      <br>
         <div class="container">
             <div class= "row justify-content-md-center">
         <button type="button" class="btn-lg btn-primary"> SEND MESSAGE</button>
@@ -26,7 +38,7 @@
 
 <style>
   .input-style{
-      
+
   }
 </style>
 @endsection
