9b55e704317495f4f3664259921a9bb174d4f962
Miguelhito <57653360+miguelhito54@users.noreply.github.com>
2020-03-31 13:02:59 -0500

    Revert "Revert "Merge branch 'master' into Miguel""
    This reverts commit 7860175d359554b7b857c85356e3088dc85b636c.

--- beauty-api/resources/views/flights.blade.php
+++ beauty-api/resources/views/flights.blade.php
@@ -13,15 +13,8 @@
 <br><br>
 
 <h2 class="text-center subtitles text-light">Find your Best Flight</h2>
-<div class="container">
-    <div class="row">
-        <div class="col-12 col-sm-12 col-md-2 col-xl-2"></div>
-        <div class="col-12 col-sm-12 col-md-8 col-xl-8 fli">
-            <iframe scrolling="yes" style="position:sticky; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:0;" src="http://old.travelpayouts.com/widgets/0a549d7d65737569518aac55ca623554.html?v=1916"></iframe>
-            <br>
-        </div>
-        <div class="col-12 col-sm-12 col-md-2 col-xl-2"></div>
-    </div>
-</div>
+
+<div class="fli"><script charset="utf-8" src="//old.travelpayouts.com/widgets/0a549d7d65737569518aac55ca623554.js?v=1919" async></script></div>
+
 
 @endsection
