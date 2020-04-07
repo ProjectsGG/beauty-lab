f1a36c1ee5c2250daa9d968c9cc2cb0526233911
ProjectsGG <59677710+ProjectsGG@users.noreply.github.com>
2020-04-06 22:12:40 -0500

    commit policy

---
+++ beauty-api/resources/views/privacypolicy.blade.php
@@ -0,0 +1,78 @@
+@extends('layout.app')
+@section('content')
+
+<br>
+<hr>
+<h1 class="text-center text-light"></h1>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+    Your privacy is important to the Beauty Lab App. Please review our privacy policy below to learn how we collect and use information. If you have any questions or concerns, we welcome you to contact us via our contact form. For further information on your privacy rights, please see the Office of the Australian Information Commissioner website.'
+    + '<br><br>What is personal information?'
+    + '<br><br>Personal information is information that identifies you or could identify you. Your name and address are obvious examples. Personal information can also include medical records, bank account details, photographs, videos, and even information about your opinions and where you work – basically, any information where you are reasonably identifiable.   '
+    + '<br><br>Strictly speaking, the information collected automatically through web browsers when you visit a website is not personal information. However, we have included our policy on how we deal with that information here for your reference.'
+    + '<br><br>Information automatically collected from Website Visitors'
+    + '<br><br>Like most website and online service providers, Beauty Lab App collects non-personally-identifying information that web browsers and search engines make available. Typically this information includes items like language, date and time of visit, time spent on the website, the type of device used (like a smart phone) and language used.'
+    + '<br><br>Our purpose in collecting this information is to better understand our visitors to improve the products and services we offer.    '
+    + '<br><br>We may also collect information like Internet Protocol (IP) addresses. This information is used as part of a moderation process to protect registered users from online abuse and harassment and confirm online purchase authenticity. Beauty Lab App reserves the right to block any IP address from access if it has been deemed to have breached the Terms and Conditions of use of this website.    '
+    + '<br><br>Aggregated Statistics'
+    + '<br><br>Beauty Lab App may collect statistics about the behaviour of visitors to its websites, including but not limited to monitoring the most popular content on the site, identifying spam or have information scanned by a third party product to identify spam and inappropriate content.    '
+    + '<br><br>Cookies'
+    + '<br><br>A cookie is a stored packet of information that is recorded on your computer when you visit a website. The website visited may associate information with that cookie each time you return to the website.'
+    + '<br><br>We use cookies to make navigation within Beauty Lab App and use of our services more user-friendly. If you do not wish to have cookies used, we recommend that you change your browser settings to refuse cookies before accessing our website. Some features of our website may not function properly without cookies enabled.'
+    + '<br><br>Advertising'
+    + '<br><br>Advertisements appearing on any of our websites may be delivered to users by advertising partners, who may use cookies. This information allows advertising networks to deliver targeted advertisements that they believe will be of most interest to you. This Privacy Policy covers the use of cookies by Beauty Lab App and does not cover the use of cookies by any advertisers.'
+    + '<br><br>Gathering of Personal Information'
+    + '<br><br>We provide opportunities for you to interact with Beauty Lab App in ways that requires the collection of some personally-identifying information. For example, you may opt to receive email based newsletters and information, or participate in an online forum discussion requiring a username. In those circumstances, we collect the information we deem necessary and appropriate to provide you with that service.    '
+    + '<br><br>We do not disclosure your personal information to anyone outside our business except as outlined below or by order of an appropriate government or law enforcement agency. Beauty Lab App reserves the right to deny you service or interaction with sections of Beauty Lab App if you chose not to supply the requested information.'
+    + '<br><br>You understand that if you create a profile for use in the forum or interaction with other users of our website you user name, profile image and comments will be publicly available and may be indexed by search engines. Always use caution when giving out any personally identifying information about yourself anywhere on the internet.'
+    + '<br><br>Use of Personal Information'
+    + '<br><br>Beauty Lab App discloses potentially personally-identifying information of website visitors to employees, contactors and affiliated organisations to operate, maintain and improve the website and services provided as well as communicate and interact with you. By using Beauty Lab App, you consent to the use and transfer of your information for this purpose.'
+    + '<br><br>We will not rent or sell personal information. Beauty Lab App will potentially disclose information in response to a court order or other governmental request if required to by Law or as recommended by legal advisors. Beauty Lab App may also disclose personally-identifying information when it believes in good faith that disclosure is necessary to protect the property or rights of Beauty Lab App, third parties, or the public.'
+    + '<br><br>If you have registered with Beauty Lab App and have supplied your email address, we may occasionally send you emails regarding our business or to request your feedback. If you send us a request (for example support request or correction request), we reserve the right to publish it in order to help clarify or respond to it, or to assist other visitors to the Beauty Lab App.'
+    + '<br><br>Personal Information and Service Providers'
+    + '<br><br>From time to time, we might establish a business relationship with other persons or entities whom we believe trustworthy and whom we have asked to confirm that their privacy practices are consistent with ours (“Service Providers”). For example, we may contract with Service Providers to provide certain services, such as credit card processing, advertising, analytics tools, widget providers, data management services, Web hosting, and Web development. We provide our Service Providers with the information reasonably necessary for them to perform these services.'
+    + '<br><br>Protection of Personal Information'
+    + '<br><br>Beauty Lab App takes commercially reasonable measures to protect against the unauthorised access, use, alteration or destruction of personal information. Beauty Lab App uses software and services provided by third-party providers with the understanding that the products in use appropriately protect personal information.'
+    + '<br><br>Business Transfers'
+    + '<br><br>If Beauty Lab App or its assets were sold, or in the unlikely event that Beauty Lab App goes out of business, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that a transfer could occur, and that any gaining Beauty Lab App may continue to use your personal information as set out in this policy.'
+    + '<br><br>Credit Card Information'
+    + '<br><br>Credit card details are only used when placing an order, are processed by our Service Providers and at no time collected or stored by Beauty Lab App. For further information on protection of credit card details, see the relevant service providers’ website.'
+    + '<br><br>Links'
+    + '<br><br>This website contains links to other sites; these links are meant for your convenience only. Please be aware that we are not responsible for the privacy practices of other websites. You should read the privacy statements of each and every website that collects your personal information.'
+    + '<br><br>Children’s Privacy Statement'
+    + '<br><br>We do not knowingly collect personal information from children under the age of 18. If we become aware that we have inadvertently received personal information from a child under the age of 18, we will delete the information from our records.'
+    + '<br><br>Change in Privacy Policy'
+    + '<br><br>As we plan to ensure our privacy policy remains current, this policy is subject to change. Please return periodically to review our privacy policy.'
+    + '<br><br>How to Contact Us'
+    + '<br><br>If you have any questions or concerns regarding our Privacy Policy or our use of your information, or believe your information needs to be updated, please use the contact form to let us know.'
+    + '<br><br>Forum Rules'
+    + '<br><br>By using the Beauty Lab App Forums you agree to abide by these Forum Rules and the terms and conditions of use of our websites.'
+    + '<br><br>The Beauty Lab App Forums have been made available to enable you to find the support and friendship that makes the process of deciding whether or not you want to have a plastic or cosmetic surgery, and/or non-surgical procedure, and the results, more rewarding.'
+    + '<br><br>Participation'
+    + '<br><br>To participate in the Forums you must create an account. You can sign up here. You may choose your own public profile. You must not impersonate any other person, nor allow another person to use your account to submit or access Forum content. Your personal details will not be disclosed unless in accordance with relevant privacy laws.'
+    + '<br><br>You can stop participating in the Forums at any time. We may remove your access to the Forums at our discretion.'
+    + '<br><br>Passwords'
+    + '<br><br>You are responsible for taking all reasonable steps to ensure that no unauthorised person has access to your Beauty Lab App passwords or accounts. You must promptly inform Beauty Lab App if you believe your account or password has been compromised or if there is any other reason you need to deactivate a password. Use the contact form to let us know you need to close an account.'
+    + '<br><br>Your Responsibility'
+    + '<br><br>Each participant in the Forums is personally responsible and liable for their submitted content and all consequences arising from their participation in the Forums. We will not be liable to you or any third parties for any action, claim, loss or damage arising from use of the Forum.'
+    + '<br><br>You must not to use the Forums to submit:'
+    + '<li>content that would violate any applicable law;'
+    + '<ul>content that is offensive, indecent or objectionable in any way;</ul>'
+    + '<ul>content to disrupt the normal flow of dialogue, or post comments that are not related to the topic being discussed, unless it is clear the discussion is free-form;</ul>'
+    + '<ul>unsolicited or unauthorised advertising, promotional material, ‘spam’, chain letters or any other form of solicitation;</ul>'
+    + '<ul>content that infringes any copyright, trademark, trade secret or other proprietary rights of any party;</ul>'
+    + '<ul>any content that the sender does not have a right to transmit under any contractual or fiduciary relationship (such as confidential information or personal information);</ul>'
+    + '<ul>any material that contains software viruses or any other computer programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment; or links to websites of a commercial nature or links to websites of individual members.</ul></li>'
+    + 'Our Responsibility'
+    + '<br><br>Beauty Lab App reserves the right (but is not obligated) to do any or all of the following:    '
+    + '<li><ul>Monitor, edit, or disclose any communication in the Forums.</ul>'
+    + '<ul>Record the dialogue in Forums</ul>'
+    + '<ul>Investigate an allegation that a communication does not conform to the Forum Rules and determine in our sole discretion to remove or request the removal of the communication.</ul>'
+    + '<ul>Remove communications which are abusive, illegal, or disruptive or that otherwise fail to conform to the terms and conditions.</ul>'
+    + '<ul>Terminate any person’s access to the Forums.</ul></li>',
+        </div>
+    </div>
+</div>
+
+@endsection
