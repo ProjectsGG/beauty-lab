39c16b44ae36007edde5c3f4c91cdc32775504e0
AndresFelipeGG <51917154+AndresFelipeGG@users.noreply.github.com>
2020-04-30 18:07:29 -0500

    tumblr

--- beauty-app/src/app/pages/blog/blog.page.ts
+++ beauty-app/src/app/pages/blog/blog.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TumblrService } from '../../services/tumblr.service';
+import { NavController } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.page.scss'],
 })
 export class BlogPage implements OnInit {
+  loading = false;
+  data: any = {
+    blog: {
+      avatar: [
+        {
+          url: '',
+        }
+      ]
+    },
+    posts: []
+  };
 
-  constructor() { }
+  constructor(private service: TumblrService, private navCtrl: NavController, private iab: InAppBrowser) { }
 
   ngOnInit() {
+    this.getData();
   }
 
+  getData() {
+    this.service.getPosts().subscribe((r: any) => {
+      this.data = r.response;
+    });
+  }
+
+  openTumblr() {
+    this.iab.create('https://beautylabapp.tumblr.com/', '_blank');
+  }
 }
